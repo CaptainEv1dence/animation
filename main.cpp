efdc5ec9ecb87b7d86164d4b332b92ac9667aa0b
petrk <kovalev.pa@phystech.edu>
2021-09-18 10:50:10 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class DiscreteState {
+private:
+    int const state;
+
+public:
+    DiscreteState(int state): state(state) { }
+
+    bool contains(int s) const {
+        return s == state;
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
